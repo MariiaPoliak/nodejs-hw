aca0716ff1ceea63d0d1c6fbad627407c33e3de5
Mariia Poliak <mariiapoliak1997@gmail.com>
2026-09-04 09:23:01 +0200

    Last fix

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -1,69 +1,19 @@
-import createHttpError from 'http-errors';
+import createError from 'http-errors';
 import { Note } from '../models/note.js';
 
-export const getAllNotes = async (req, res, next) => {
-  try {
-    const notes = await Note.find();
-    res.status(200).json(notes);
-  } catch (error) {
-    next(error);
-  }
-};
-
-export const getNoteById = async (req, res, next) => {
+export const updateNote = async (req, res, next) => {
   try {
     const { noteId } = req.params;
-    const note = await Note.findById(noteId);
-
-    if (!note) {
-      throw createHttpError(404, 'Note not found');
-    }
-
-    res.status(200).json(note);
-  } catch (error) {
-    next(error);
-  }
-};
-
-export const createNote = async (req, res, next) => {
-  try {
-    const note = await Note.create(req.body);
-    res.status(201).json(note);
-  } catch (error) {
-    next(error);
-  }
-};
-
-export const patchNote = async (req, res, next) => {
-  try {
-    const { noteId } = req.params;
-    const updatedNote = await Note.findByIdAndUpdate(noteId, req.body, {
+    const note = await Note.findByIdAndUpdate(noteId, req.body, {
       returnDocument: 'after',
       runValidators: true,
     });
 
-    if (!updatedNote) {
-      throw createHttpError(404, 'Note not found');
+    if (!note) {
+      throw createError(404, 'Note not found');
     }
 
-    res.status(200).json(updatedNote);
-  } catch (error) {
-    next(error);
-  }
-};
-
-export const updateNote = patchNote;
-
-export const deleteNote = async (req, res, next) => {
-  try {
-    const { noteId } = req.params;
-    const deletedNote = await Note.findByIdAndDelete(noteId);
-
-    if (!deletedNote) {
-      throw createHttpError(404, 'Note not found');
-    }
-
-    res.status(200).json(deletedNote);
+    res.status(200).json(note);
   } catch (error) {
     next(error);
   }
